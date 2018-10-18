b0bcc1d33cffffea02a3231bdbfa912f7a89fa48
Alex Kharouk <akharoukpp@gmail.com>
2018-10-18 12:34:56 +0100

    add unique id counter

--- app/eNotes/note_model.js
+++ app/eNotes/note_model.js
@@ -1,9 +1,14 @@
-class Note {
-  constructor(note) {
-    this.note = note;
-  }
+(function(exports) {
+  let idCounter = 0;
+  class Note {
+    constructor(note) {
+      this.note = note;
+      this.id = idCounter++;
+    }
 
-  getNote() {
-    return this.note;
+    getNote() {
+      return this.note;
+    }
   }
-}
+  exports.Note = Note;
+})(this);
--- app/eNotes/tests/note_model_pool.js
+++ app/eNotes/tests/note_model_pool.js
@@ -5,6 +5,13 @@ barracuda.describe("Notes Class", function() {
     barracuda.expect(typeof Note).toEqual("function");
   });
 
+  barracuda.it("has a unique Id", function() {
+    const note = new Note("hey");
+    const note2 = new Note("yooo");
+    barracuda.expect(note.id).toEqual(0);
+    barracuda.expect(note2.id).toEqual(1);
+  });
+
   barracuda.it("can get the note", function() {
     const newNote = new Note("test");
     barracuda.expect(newNote.getNote()).toEqual("test");
